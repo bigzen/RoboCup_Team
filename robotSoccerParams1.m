6703af4dc5b704127966efb8a376df2b3909453d
Rajit Roy <83898887+rajit1995@users.noreply.github.com>
2023-02-14 22:59:21 +0000

    Update robotSoccerParams1.m

--- robotSoccerParams1.m
+++ robotSoccerParams1.m
@@ -27,7 +27,7 @@ sampleTime = 0.1;
 
 % Robot definitions and dimensions
 numRobots = 8;
-robotRadius = 0.6;
+robotRadius = 0.3;
 wheelRadius = 0.2;
 robotColors = [1 0 0;0 0 1;1 0 0;0 0 1;1 0 0;0 0 1;1 0 0;0 0 1];
 
@@ -52,8 +52,8 @@ ballVelNoise = 0.5;
 ballAngleNoise = pi/24;
 
 ballThresh = robotRadius + 0.3; % Distance to grab the ball
-ballCarryFactor = 0.9;          % Speed penalty when carrying the ball
-outOfBoundsDist = 2;            % Distance to place ball back in bounds
+ballCarryFactor = 0.94;          % Speed penalty when carrying the ball
+outOfBoundsDist = 3;            % Distance to place ball back in bounds
 
 % Randomize initial conditions
 randomizeStartingPositions;
@@ -94,8 +94,8 @@ defenderAwayPoses = [7.2 2.85 pi;
 defenderKickSpeed = 5;              % Kick speed for defending
 
 % Goalkeeper parameters
-goalkeeperPoses = [ 2.0 2.35 0;
-                   9.0 2.35 pi]';    % Goalkeeper poses for home and away teams
+goalkeeperPoses = [ 0.0 3.0 0;
+                   9.0 3.0 pi]';    % Goalkeeper poses for home and away teams
 goalkeeperKickSpeed = 10;           % Kick speed for goalkeeping
 
 
@@ -112,11 +112,8 @@ pen_nr_kicks = 5;
 start_goal_l = 0;
 start_goal_r = 0;
 ball_accel_max = 2.7;
-ball_decay = 0.94;
-ball_rand = 0.05;
 ball_size = 0.085;
 ball_speed_max = 3;
-ball_stuck_area = 3;
 ball_weight = 0.2;
 inertia_moment = 5;
 kick_power_rate = 0.027;
@@ -137,7 +134,6 @@ minneckang = -90;
 minneckmoment = -180;
 minpower = -100;
 offside_active_area_size = 2.5;
- = ;
 offside_kick_margin = 9.15;
 offside_kick_margin = 9.15;
 pen_dist_x = 42.5;
@@ -145,7 +141,6 @@ pen_max_goalie_dist_x = 14;
 player_accel_max = 1;
 player_decay = 0.4;
 player_rand = 0.1;
-player_size = 0.3;
 player_speed_max = 1.05;
 player_speed_max_min = 0.75;
 player_weight = 60;
