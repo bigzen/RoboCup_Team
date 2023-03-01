144ff79343fb65e3b3e9a78af90fc04c77893d8b
SamuelAo98 <123199324+SamuelAo98@users.noreply.github.com>
2023-03-01 18:16:41 +0000

    Add files via upload

---
+++ KickBall.m
@@ -0,0 +1,22 @@
+function [ball_speed_x,ball_speed_y]=KickBall(ball_x, ball_y,ball_speed,angle_to_ball)
+% 设定kick ball的力度
+kick_power = 30;
+
+        % 计算kick ball的方向和力度
+       
+        kick_angle = 2*pi*rand()-pi;
+        kick_speed_x = kick_power * cos(kick_angle);
+        kick_speed_y = kick_power * sin(kick_angle);
+        
+        % 更新球的速度
+        %update the speed of the ball
+        ball_speed_x=ball_speed(1);
+        ball_speed_y=ball_speed(2);
+        ball_speed_x = ball_speed_x + kick_speed_x;
+        ball_speed_y = ball_speed_y + kick_speed_y;
+
+    
+
+
+
+
