cb709564ecb838fd247a661dc182d83114f337b0
Mochimo-mo <59297918+Mochimo-mo@users.noreply.github.com>
2023-03-01 17:57:37 +0000

    Add files via upload

--- ball.m
+++ ball.m
@@ -1,28 +1,20 @@
 classdef ball
 
     properties
-        pos_x
-        pos_y
-        color
+        %position of ball
+        pos
+        %velocity of ball
+        vel
+        %direction of ball
+        dir
     end
     
     methods
-        function ball = ball(pos)
-            ball.color = 'w';
-            ball.pos_x = pos(1);
-            ball.pos_y = pos(2);
-            display_ball(ball,ball.pos_x,ball.pos_y)
-        end
-        function display_ball(ball, x, y)
-            ball.pos_x = x;
-            ball.pos_y = y;
-
-            y_circle = y;
-            x_circle = x;
-            
-            radius=0.143;%the radius of a ball
-            circle(x_circle,y_circle,radius,ball.color)
-            hold on 
+        function ball = getballInit(ball,initBallPos)
+            ball.pos = initBallPos;
+            ball.vel = 0;
+            ball.dir = 0;
+   
         end
     end
 end
