d057c74e87c9592a3760faf0d33216c705f9f8bb
2692726g <2692726g@LIB-X3-032.campus.gla.ac.uk>
2023-03-10 18:16:41 +0000

    created struct array for updates

--- updateGameState.m
+++ updateGameState.m
@@ -2,6 +2,27 @@ function gameState = updateGameState(gameState)
 %UPDATEGAMESTATE Summary of this function goes here
 %   Detailed explanation goes here
 %% call the plyer action function and store the variables
+update = struct('id',[], ...
+                 'ball', [],...
+                 'player', []);
+for id=1:8
+    if gameState.players(id).role==0
+        [ball_speed, player_acceleration] = goal_keeper(gameState.ball,gameState.players,id);
+        update(id).id=id;
+        update(id).ball=ball_speed;
+        update(id).player=player_acceleration;
+    elseif gameState.players(id).role==1
+        [ball_speed, player_acceleration] = defender(gameState.ball,gameState.players,id);
+        update(id).id=id;
+        update(id).ball=ball_speed;
+        update(id).player=player_acceleration;
+    else
+        [ball_speed, player_acceleration] = attacker(gameState.ball,gameState.players,id);
+        update(id).id=id;
+        update(id).ball=ball_speed;
+        update(id).player=player_acceleration;
+    end
+end
 
 %% calculate new player position and speed update the game state
 
