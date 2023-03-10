43a4b1df424189bd6b5ebf6d3b1bbb6274fab15e
2692726g <2692726g@LIB-X3-032.campus.gla.ac.uk>
2023-03-10 17:57:33 +0000

    sections created

--- updateGameState.m
+++ updateGameState.m
@@ -1,7 +1,13 @@
-function [outputArg1,outputArg2] = updateGameState(inputArg1,inputArg2)
+function gameState = updateGameState(gameState)
 %UPDATEGAMESTATE Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+%% call the plyer action function and store the variables
+
+%% calculate new player position and speed update the game state
+
+
+%% calculate ball position and decide who kickedlast 
+
+%%
 end
 
