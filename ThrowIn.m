78c25e68f583cf708f9ae02cd5a3e09060fde54f
Ching Hsuan(Luca) Lin <67009502+Lucaz0619@users.noreply.github.com>
2023-04-16 21:06:11 +0100

    Add files via upload

--- ThrowIn.m
+++ ThrowIn.m
@@ -1,4 +1,18 @@
-function gameState = ThrowIn(gameState,advteam) 
+function gameState = ThrowIn(gameState,advteam)
+    %% Caculate the ThrowIn ball position
+    throwInPos = [0 0];
+    if gameState.ball.position(2) > 6
+        slope = (gameState.ball.prev_pos(2)-gameState.ball.position(2))/(gameState.ball.prev_pos(1)-gameState.ball.position(1));
+        throwInPos(1) = gameState.ball.prev_pos(1) + (6 - gameState.ball.prev_pos(2)) / slope;
+        throwInPos(2) = 6;
+    else
+        slope = (gameState.ball.prev_pos(2)-gameState.ball.position(2))/(gameState.ball.prev_pos(1)-gameState.ball.position(1));
+        throwInPos(1) = gameState.ball.prev_pos(1) + (0 - gameState.ball.prev_pos(2)) / slope;
+        throwInPos(2) = 0;
+    end
+    
+    gameState.ball.position = throwInPos;
+
     if advteam == 0
         thruplayeridx = randsample([2 4 3], 1);
         if (gameState.ball.position(1) > 0 && gameState.ball.position(1) <=4.5) && gameState.ball.position(2) <= 0
